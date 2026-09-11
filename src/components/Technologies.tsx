069fc8d5fe78baa56789f9a40a8149c9904fe123
Mohiuddin <mohiuddinn714@gmail.com>
2026-09-12 00:02:22 +0600

    add selected stack section and add functionality for how many stack are selected .

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,57 +1,64 @@
 import { use, useState } from "react";
 import type { ITechnologyType } from "../types/TechnologyType";
 import Technology from "./Technology";
-import TechnologyCart from './TechnologyCart'
+import TechnologyCart from "./TechnologyCart";
 
-export interface TechnologiesProps { 
+export interface TechnologiesProps {
   technologiesPromise: Promise<ITechnologyType[]>;
 }
 
-export default function Technologies({ technologiesPromise }: TechnologiesProps) {
+export default function Technologies({
+  technologiesPromise,
+}: TechnologiesProps) {
+  const [technologyCart, setTechnologyCart] = useState<ITechnologyType[]>([]);
 
-   const [technologyCart,setTechnologyCart] = useState<ITechnologyType[]>([])
+  const technologies = use(technologiesPromise);
 
-   const technologies = use(technologiesPromise)
+  return (
+    <section className="container mx-auto px-6 lg:px-8 py-2">
+      <div className="mb-10">
+        <h1 className="text-[#0F172A] font-black text-4xl font-inter mb-2">
+          Explore the{" "}
+          <span className="bg-linear-to-r from-[#EC4899] to-[#8B5CF6] bg-clip-text text-transparent">
+            Technologies
+          </span>
+        </h1>
+        <p className="text-[#64748B] font-jakarta tracking-[0.9px] text-lg">
+          Pick one technology per category to build your ideal stack.
+        </p>
 
-   console.log(technologyCart)
-   
-   return (
-     <section className="container mx-auto px-6 lg:px-8 py-2">
-       <div className="mb-10">
-         <h1 className="text-[#0F172A] font-black text-4xl font-inter mb-2">
-           Explore the{" "}
-           <span className="bg-linear-to-r from-[#EC4899] to-[#8B5CF6] bg-clip-text text-transparent">
-             Technologies
-           </span>
-         </h1>
-         <p className="text-[#64748B] font-jakarta tracking-[0.9px] text-lg">
-           Pick one technology per category to build your ideal stack.
-         </p>
+        <div></div>
+      </div>
 
-         <div></div>
-       </div>
+      <div className="grid grid-cols-12 gap-8">
+        {/* Stack show side */}
+        {/* 75% / col span 9 */}
+        <div className="col-span-9 grid grid-cols-3 items-center gap-5">
+          {technologies.map((technology) => (
+            <Technology
+              key={technology.id}
+              technology={technology}
+              setTechnologyCart={setTechnologyCart}
+            />
+          ))}
+        </div>
 
-       <div className="grid grid-cols-12 items-center gap-8">
-         {/* Stack show side */}
-         {/* 75% / col span 9 */}
-         <div className="col-span-9 grid grid-cols-3 items-center gap-5">
-           {technologies.map((technology) => (
-             <Technology
-               key={technology.id}
-               technology={technology}
-               setTechnologyCart={setTechnologyCart}
-             />
-           ))}
-         </div>
-
-         {/* Selected stack side */}
-         {/* 25% / col span 3 */}
-         <div className="col-span-3">
-           {technologyCart.map((technology) => (
-             <TechnologyCart key={technology.id} technology={technology} />
-           ))}
-         </div>
-       </div>
-     </section>
-   );
+        {/* Selected stack side */}
+        {/* 25% / col span 3 */}
+        <div className="col-span-3 p-5 rounded-2xl border border-[#F1F5F9] flex justify-between flex-col">
+          <div>
+            <h1 className="font-bold text-[16px] font-inter text-[#0F172A]">
+              Your Stack
+            </h1>
+            <p className="font-jakarta text-[#94A3B8] text-[12px] ">
+              {technologyCart.length} Technology Selected
+            </p>
+          </div>
+          {technologyCart.map((technology) => (
+            <TechnologyCart key={technology.id} technology={technology} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/TechnologyCart.tsx
+++ src/components/TechnologyCart.tsx
@@ -1,14 +1,21 @@
 import type { ITechnologyType } from "../types/TechnologyType"
 
 export interface StackCartProps {
-   technology: ITechnologyType
+  technology: ITechnologyType;
 }
 
 export default function StackCart({ technology }: StackCartProps) {
-   
-   return (
+  return (
+    <div>
       <div>
-         
+        <div></div>
       </div>
-   )
+
+      <div className="w-full">
+        <button className="w-full border border-[#ED8C85] text-[14px] font-inter font-semibold rounded-lg">
+          Remove All
+        </button>
+      </div>
+    </div>
+  );
 }
